e8fd8bb7126f27bc4cd7573da765043db5c1f886
Chaim Kram <chaimk@umich.edu>
2014-08-04 17:59:34 +0300

    Added a created_this_week? flag

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -1,6 +1,11 @@
 class Deed < ActiveRecord::Base
-  attr_accessible :activity_id, :person_id, :points
+	attr_accessible :activity_id, :person_id, :points
 
-  belongs_to :person
-  belongs_to :activity
+	belongs_to :person
+	belongs_to :activity
+
+	def created_this_week?
+		Date.current.beginning_of_week == created_at.beginning_of_week
+	end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,9 @@ module ActivitiesApp
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    # Set the beginning of the week to Sunday:
+    config.beginning_of_week = :sunday
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
