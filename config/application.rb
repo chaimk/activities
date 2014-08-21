c9eb3e9730dc6259542a0a12d350feb25534c959
Chaim Kram <chaimk@umich.edu>
2014-08-21 19:22:15 +0300

    Still trouble in heroku-land; fiddling with application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,16 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-if defined?(Bundler)
+# if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
-end
+#end
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module ActivitiesApp
   class Application < Rails::Application
