a5819fc1bbb1baf5009dd5a4416e6fce38ee9335
Chaim Kram <chaimk@umich.edu>
2014-08-21 22:25:38 +0300

    fucking around with production.rb to get git push heroku to work. ugh.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,9 +18,9 @@ ActivitiesApp::Application.configure do
   config.assets.js_compressor  = :uglifier
   # config.assets.css_compressor = :sass
   
-  # Don't fallback to assets pipeline if a precompiled asset is missed
+  # Fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
+  # config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs
   config.assets.digest = true
