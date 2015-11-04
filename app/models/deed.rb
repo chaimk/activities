3db5bf8242fd10a1e53afb6aa925622816b70fe8
Chaim Kram <chaimk@umich.edu>
2015-11-04 22:32:15 +0200

    Some validation stuff; tweaked default scope sort order on Deeds to use performed_at instead of created_at

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -1,9 +1,11 @@
 class Deed < ActiveRecord::Base
 	attr_accessible :activity_id, :person_id, :points, :performed_at
   
-  # validates :performed_at, presence: true
-
-  default_scope { order(created_at: :desc) }
+  validates :performed_at, presence: true
+  validates :points, presence: true, 
+    numericality: { only_integer: true, greater_than: 0, less_than: 15 }
+  
+  default_scope { order(performed_at: :desc) }
 
 	belongs_to :person
 	belongs_to :activity
